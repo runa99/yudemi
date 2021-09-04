64bc37006f7479270eda8c4dbcbd6552507401a4
runa99 <yuyu0330aki@gmail.com>
2021-09-05 00:42:09 +0900

    内容修正

--- aki.py
+++ aki.py
@@ -1,4 +1,6 @@
 aki=['あき','はる','なつ','ふゆ']
+aki.remove('あき') #remove関数の出力の確認
+print(aki,'remove関数の出力の確認')
 #リストakiを出力
 print(aki)
 #リストakiのリストは何個あるか
@@ -6,16 +8,14 @@ print(len(aki))
 
 #リストakiの０番目　
 print(list(aki)[0])
-
 #リストakiの２番目
 print(aki[2])
 
-#リストakiの３番目
-print(aki[3])
 #リストakiの最後の要素を取り出す
-#print(aki.pop())
+print(aki.pop())
 #リストakiの最後の要素を取り出す
 print(aki[-1])
+
 
 aki.append("ごっかん")
 print(aki)
--- for_bun.py
+++ for_bun.py
@@ -8,8 +8,8 @@
 #str(2*3)
 #print(str(2*3))
 
-for x in range(1,10): #レンジ1から１０をXに取り出す
-    for y in range(1,10): #レンジ１から１０をYに取り出す
+for x in range(1,10): #レンジ1から9をXに取り出す
+    for y in range(1,10): #レンジ１から9をYに取り出す
         print(str(x) + 'x' + str(y) + '=' + str(x*y))
         print(type(x))
         
