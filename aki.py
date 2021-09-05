d25f9ac3253a05b2f07d0e8b5eb8f279a5c7a674
runa99 <yuyu0330aki@gmail.com>
2021-09-05 20:27:14 +0900

    フォーマット修正

--- aki.py
+++ aki.py
@@ -1,19 +1,19 @@
-aki=['あき','はる','なつ','ふゆ']
-aki.remove('あき') #remove関数の出力の確認
-print(aki,'remove関数の出力の確認')
-#リストakiを出力
+aki = ['あき', 'はる', 'なつ', 'ふゆ']
+aki.remove('あき')  # remove関数の出力の確認
+print(aki, 'remove関数の出力の確認')
+# リストakiを出力
 print(aki)
 # リストakiのリストは何個あるか
 print(len(aki))
 
 # リストakiの０番目　
 print(list(aki)[0])
-#リストakiの２番目
+# リストakiの２番目
 print(aki[2])
 
-#リストakiの最後の要素を取り出す
+# リストakiの最後の要素を取り出す
 print(aki.pop())
-#リストakiの最後の要素を取り出す
+# リストakiの最後の要素を取り出す
 print(aki[-1])
 
 
